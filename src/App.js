7a330e768ee73b113d16fd15851e998ca4e7fa57
Rifo Genadi <rifoagenadi@gmail.com>
2019-04-28 09:02:46 +0700

    Add browser router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const componentList = {
+  'Destination': import('./components/Destination'),
+  'Home': import('./components/Home'),
+  'Detail': import('./components/Detail'),
+  'Voucher': import('./components/Voucher')
+};
+
+const Destination = setUpLoadable('Destination');
+const Home = setUpLoadable('Home');
+const Detail = setUpLoadable('Detail');
+const Voucher = setUpLoadable('Voucher');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ScrollToTop timeout={200}>
+      <RouteWrapper timeout={400}>
+        <Layout navbar exact path="/" component={Home} />
+        <Layout navbar exact path="/destination" component={Destination} />
+        <Layout navbar exact path="/voucher" component={Voucher} />
+        <Layout navbar exact path="/detail" component={Detail} />
+        {/* <Layout navbar exact path="/404" component={NoMatch} />
+        <Layout navbar exact component={NoMatch} /> */}
+      </RouteWrapper>
+    </ScrollToTop>
+  </BrowserRouter>
   );
 }
 
